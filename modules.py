928dc88f76bffdc0af69e5aaa7eba8d65a284df3
OJKotkaLKP <ollikarmeniemi2025@proton.me>
2025-07-27 00:28:49 +0300

    Update modules.py

--- modules.py
+++ modules.py
@@ -1,1 +1,22 @@
-
+import bluetooth
+from prettytable import PrettyTable
+class BluetoothScanner:
+    def discover(self, scan_type="all"):
+        devices = []
+        
+        if scan_type in ["all", "bt"]:
+            print("[*] Discovering Bluetooth devices...")
+            bt_devices = bluetooth.discover_devices(lookup_names=True)
+            for addr, name in bt_devices:
+                devices.append(("Bluetooth", name, addr))
+        
+        # Implementation for other types would go here
+        
+        if devices:
+            table = PrettyTable()
+            table.field_names = ["Type", "Device Name", "Address"]
+            for device in devices:
+                table.add_row(device)
+            print(table)
+        else:
+            print("[-] No devices found")
